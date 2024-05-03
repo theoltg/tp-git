49717b8a1683a52aa4ce7964c400f87d00234db0
theoltg <theo.letang@gmail.com>
2024-05-03 17:08:09 +0200

    Ajout Rotation function

--- ServoMotor.py
+++ ServoMotor.py
@@ -1,9 +1,3 @@
 class ServoMotor:
     def __init__(self,pinV,pinCOM,pinPMW, angle):
-        self.pinV=pinV
-        self.pinCOM=pinCOM
-        self.pinPMW=pinPMW
-        self.angle=None
-        
-    def Rotation(self):
-        print("Rotation at {self.angle}°")
+     
